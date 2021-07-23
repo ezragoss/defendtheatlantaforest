8485d8c9a9d598384c87a7e3c1e0061bfb686579
Ezra Goss <ezragoss@Ezras-MacBook-Pro.local>
2021-07-22 21:00:43 -0700

    Changed donation link to opencollective

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ const IndexPage = () => (
     <h1 className={"body__motto"}>no cop city. no hollywood dystopia.</h1>
     <br/>
     <a className={"button button-primary button-large button-wide"}
-            href={"https://actionnetwork.org/fundraising/forest-justice-defense-fund"}>
+            href={"https://opencollective.com/forest-justice-defense-fund"}>
       Donate to Defend the Forest
     </a>
     <br/>
